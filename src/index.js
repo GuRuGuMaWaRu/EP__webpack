68c10b55ccbb4ea56e67e6f4618988be65135270
Peter Krevenets <peter.k.nets@gmail.com>
2018-11-03 11:43:12 +0200

    Use dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import _ from "lodash";
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ "lodash")
+    .then(({ default: _ }) => {
+      const element = document.createElement("div");
 
-if (process.env.NODE_ENV !== "production") {
-  console.log("Looks like we are in development mode!");
+      element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+      return element;
+    })
+    .catch(error => "An error occurred while loading the component");
 }
 
-function component() {
-  const element = document.createElement("pre");
-
-  element.innerHTML = [
-    "Hello webpack!",
-    "How are you?",
-    "I am fine! Thank you!"
-  ].join("\n\n");
-
-  return element;
-}
-
-document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+});
